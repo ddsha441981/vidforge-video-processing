8dd7f43566f4d1a99effe75c7cb05ef46ca9e265
ddsha441981 <ddsa44191@gmail.com>
2025-04-23 18:38:35 +0530

    GlobalController added

--- vidforge-backend/src/main/java/com/cwc/vidforge/controller/GlobalController.java
+++ vidforge-backend/src/main/java/com/cwc/vidforge/controller/GlobalController.java
@@ -1,8 +1,16 @@
 package com.cwc.vidforge.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller("/api")
 public class GlobalController {
+
+    @GetMapping
+    @ResponseBody
+    public String welcomePage() {
+        return "API working fine";
+    }
 
 }
